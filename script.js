354e4f24fe47bf899ed70b96b04367cf55704a4d
Elvis Lima <evscoder@gmail.com>
2023-07-23 11:42:43 -0300

    projeto final

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function toggleMode() {
   const html = document.documentElement
 
-  if (html.classList.contains("light")) {
+  html.classList.toggle("light")
+
+  /*  if (html.classList.contains("light")) {
     html.classList.remove("light")
   } else {
     html.classList.add("light")
   }
+   */
 
   const img = document.querySelector("#profile img")
 
